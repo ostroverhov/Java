322b4cb2aabcb1ec7a3ac8ca36f9401d037a46f2
Vlad Ostroverhov <ostroverhov.vlad@yandex.by>
2018-08-22 09:50:21 +0300

    Check last digit in number

---
+++ lesson2/CheckLast7.bat
@@ -0,0 +1,4 @@
+mkdir target
+javac -d target -encoding utf-8 src/CheckLast7.java
+java -cp target CheckLast7
+pause
---
+++ lesson2/src/CheckLast7.java
@@ -0,0 +1,9 @@
+public class CheckLast7 {
+    public static void main(String[] args) {
+        int number = 123;
+        if (number%10 == 7)
+            System.out.println("Число оканчивается на 7");
+        else
+            System.out.println("Число НЕ оканчивается на 7");
+    }
+}
