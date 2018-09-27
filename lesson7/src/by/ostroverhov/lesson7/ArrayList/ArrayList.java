8f763ccf86edfc104e920fa8a9718f06a672e25f
Vlad Ostroverhov <ostroverhov.vlad@yandex.by>
2018-09-27 22:02:55 +0200

    ArrayList lesson7

---
+++ lesson7/src/by/ostroverhov/lesson7/ArrayList/ArrayList.java
@@ -0,0 +1,105 @@
+package by.ostroverhov.lesson7.ArrayList;
+
+import java.util.Arrays;
+import java.util.ConcurrentModificationException;
+import java.util.NoSuchElementException;
+import java.util.Objects;
+import java.util.function.Consumer;
+
+public class ArrayList<T> implements List<T> {
+    private static final int DEFAULT_CAPACITY = 10;
+    private T[] array;
+    private int index = 0;
+
+    public ArrayList() {
+        array = (T[]) new Object[DEFAULT_CAPACITY];
+    }
+
+    @Override
+    public void add(T element) {
+        if (index >= array.length) {
+            T[] newArray = (T[]) new Object[array.length + DEFAULT_CAPACITY];
+            System.arraycopy(array, 0, newArray, 0, array.length);
+            array = newArray;
+        }
+        array[index] = element;
+        index++;
+    }
+
+    public T get(int index) throws IndexOutOfBoundsException {
+        if (index >= array.length)
+            throw new IndexOutOfBoundsException();
+        else return array[index];
+    }
+
+    public T remove(int index) {
+        if (index >= array.length)
+            throw new IndexOutOfBoundsException();
+        else {
+            T removeValue = array[index];
+
+            int numMoved = array.length - index - 1;
+            if (numMoved > 0) {
+                System.arraycopy(array, index + 1, array, index, numMoved);
+            }
+            array[array.length - 1] = null;
+            return removeValue;
+        }
+    }
+
+    public int size() {
+        return array.length;
+    }
+
+
+    public Iterable<T> iterable() {
+        return new Itr();
+    }
+
+    private class Itr implements Iterable<T> {
+        int lastRet = -1;
+
+        public Itr() {
+        }
+
+        @Override
+        public boolean hasNext() {
+            return index != array.length - 1;
+        }
+
+        @Override
+        public T next() {
+            int i = index;
+            if (i >= array.length)
+                throw new NoSuchElementException();
+            index = i + 1;
+            return array[lastRet = index];
+        }
+
+        @Override
+        public void remove() {
+            try {
+                ArrayList.this.remove(lastRet);
+                index = lastRet;
+            } catch (IndexOutOfBoundsException ex) {
+                throw new ConcurrentModificationException();
+            }
+        }
+
+        @Override
+        public void forEach(Consumer<? super T> action) {
+
+            for (int i = 0; i < array.length; i++) {
+                action.accept(array[i]);
+            }
+        }
+    }
+
+    @Override
+    public String toString() {
+        return "ArrayList{" +
+                "array=" + Arrays.toString(array) +
+                ", index=" + index +
+                '}';
+    }
+}
---
+++ lesson7/src/by/ostroverhov/lesson7/ArrayList/List.java
@@ -0,0 +1,8 @@
+package by.ostroverhov.lesson7.ArrayList;
+
+public interface List<T> {
+    void add(T element);
+
+
+
+}
