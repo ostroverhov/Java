08177291b4bec8a7f7dbb9bee6fb948077756566
Vlad Ostroverhov <ostroverhov.vlad@yandex.by>
2018-12-08 12:23:45 +0300

    calculator from lesson 6 with menu

--- lesson6/run.bat
+++ lesson6/run.bat
@@ -1,4 +1,4 @@
 mkdir target
 javac -d target -sourcepath src src/by/ostroverhov/lesson6/calculator/Main.java
-java -cp target -Djava.util.logging.config.file=logging.properties target.by.itacademy.lesson6.calculator.Main
+java -cp target -Djava.util.logging.config.file=logging.properties by.ostroverhov.lesson6.calculator.Main
 pause
