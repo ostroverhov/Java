9032313033104d740ad0acc72836262b62914d77
Vlad Ostroverhov <ostroverhov.vlad@yandex.by>
2018-12-09 18:15:22 +0300

    my project with H2database

---
+++ project/src/main/java/by/ostroverhov/myProject/drugs/DataSource.java
@@ -0,0 +1,40 @@
+package by.ostroverhov.myProject.drugs;
+
+import com.zaxxer.hikari.HikariConfig;
+import com.zaxxer.hikari.HikariDataSource;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.util.Properties;
+
+import static java.util.Objects.requireNonNull;
+
+public class DataSource {
+    private static final DataSource INSTANCE = new DataSource();
+    private HikariConfig config = new HikariConfig(properties());
+    private HikariDataSource ds = new HikariDataSource(config);
+
+    private DataSource(){
+
+    }
+
+    public static DataSource getInstance(){
+        return INSTANCE;
+    }
+
+    public Connection getConnection() throws SQLException{
+        return ds.getConnection();
+    }
+
+    private Properties properties() {
+        Properties properties = new Properties();
+        try (InputStream stream = requireNonNull(getClass().getClassLoader().getResource("datasource.properties")).openStream()) {
+            properties.load(stream);
+            return properties;
+        } catch (IOException e) {
+            throw new IllegalArgumentException(e);
+        }
+    }
+}
--- project/src/main/java/by/ostroverhov/myProject/drugs/DrugList.java
+++ project/src/main/java/by/ostroverhov/myProject/drugs/DrugList.java
@@ -1,32 +1,72 @@
 package by.ostroverhov.myProject.drugs;
 
-import by.ostroverhov.myProject.search.MySearchException;
-
+import java.sql.*;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.*;
+import java.util.Date;
 
 public class DrugList<T> implements Iterator<Drug> {
-    private static final Scanner SCANNER = new Scanner(System.in);
-
+    private DataSource ds = DataSource.getInstance();
     List<Drug> ourDrugs = new ArrayList<Drug>();
 
-    {
+    public List<Drug> find(String language) throws Exception {
+
+        Connection connection = null;
+        PreparedStatement pstmt = null;
+        ResultSet rs = null;
+
+        try {
+            connection = ds.getConnection();
+            pstmt = connection.prepareStatement("select * from drugs");
+            pstmt.setString(1, language);
+            rs = pstmt.executeQuery();
+            while (rs.next()) {
+                ourDrugs.add(new Drug(rs.getInt(1),
+                        rs.getString(2),
+                        rs.getString(3),
+                        rs.getString(4),
+                        rs.getDate(5),
+                        rs.getBoolean(6),
+                        rs.getInt(7)));
+            }
+        }catch (SQLException e) {
+            throw new SQLException(e);
+        } finally {
+            try {
+                if (rs != null) rs.close();
+            } catch (SQLException e) {
+                e.printStackTrace();
+            } finally {
+                try {
+                    if (pstmt != null) pstmt.close();
+                } catch (SQLException e) {
+                    e.printStackTrace();
+                } finally {
+                    try {
+                        if (connection != null) connection.close();
+                    } catch (SQLException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+        }
+
+        return ourDrugs;
+    }
+
+
+
+   /* {
         ourDrugs.add(0, new Drug(0, "bortezomib", "liophilizate", "010218", parse("2018-04-05"), false, 550));
         ourDrugs.add(1, new Drug(1, "fludarabin", "tablet", "011218", parse("2018-06-08"), true, 1350));
         ourDrugs.add(2, new Drug(2, "decitabin", "liophilizate", "010516", parse("2016-03-15"), false, 580));
         ourDrugs.add(3, new Drug(3, "carboplatin", "concentrate", "082118", parse("2018-01-14"), true, 4300));
         ourDrugs.add(4, new Drug(4, "clofarabin", "liophilizate", "051717", parse("2017-08-21"), true, 1500));
-    }
+    }*/
 
     public List<Drug> getOurDrugs() {
         return ourDrugs;
-    }
-
-    public void output() {
-        for (Drug i : ourDrugs) {
-            System.out.println(i);
-        }
     }
 
     private Date parse(String date) {
